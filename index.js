b3f4f0e0de2cef681dc468a005bea341f8f21234
Chirag <chiragk927@gmail.com>
2021-10-25 00:18:08 -0400

    error fix

--- index.js
+++ index.js
@@ -19,10 +19,11 @@ app.use((req, res, next) => {
 
 app.use('/coinbase', coinbase)
 app.use('/kraken', kraken)
-app.use(express.static(path.resolve(__dirname, '../client/build')));
+
+app.use(express.static(path.resolve(__dirname, './client/build')));
 
 app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, './client/build', 'index.html'));
 });
 
 const port = process.env.PORT || 3001;
