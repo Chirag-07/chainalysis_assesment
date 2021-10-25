19f3b8565156d8918ba2466d3188c2c60cb112d3
Chirag <chiragk927@gmail.com>
2021-10-25 00:13:44 -0400

    deployment commit

--- index.js
+++ index.js
@@ -6,14 +6,24 @@ require('dotenv').config();
 
 const app = express();
 
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+    next();
+});
+
 
 app.use('/coinbase', coinbase)
 app.use('/kraken', kraken)
-// app.use(express.static(path.resolve(__dirname, '../client/build')));
+app.use(express.static(path.resolve(__dirname, '../client/build')));
 
-// app.get('*', (req, res) => {
-//     res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
-// });
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+});
 
 const port = process.env.PORT || 3001;
 app.listen(port, ()=>console.log(`Server running on port ${port}`))
